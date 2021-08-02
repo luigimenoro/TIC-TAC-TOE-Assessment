2c142d93ee734c6e41aa4533735f159743e4a679
luigimenoro <Luigi.menoro@liston.school.nz>
2021-08-03 11:48:06 +1200

    Rebuilt the base component program.
    In this change, I have deleted all the work I have done so far to make the real base component of my game. I have thought about this in a while, since I have made several changes on my planning, and also to my functions. That is why I want to make a new start.
    
    ------------------------------------------
    
    I have added an instruction.py, in this python file,  it consists the lists of the instructions that I will be called in one of my functions that I will be using in my base component

---
+++ instruction.py
@@ -0,0 +1,47 @@
+# array for the instructions
+instructions_array = [
+    """
+    You begin by entering your element, and you 
+    are going to use that element throughout the game.
+    """,
+    """
+    Then the game will show a big 3 x 3 grid
+    """,
+    """
+    Where it will ask you where you want to put your 
+    own element to the 3 x 3 grid. 
+    """,
+    """
+    To put your own element to the grids you need to enter the number that 
+    orresponds to the spot where you want the element to be entered.
+             |     |     
+          1  |  2  |  3 
+        _____|_____|_____
+             |     |     
+          4  |  5  |  6  
+        _____|_____|_____
+             |     |     
+          7  |  8  |  9  
+             |     |     
+   ___________________________      
+
+    Just like this wins:
+             |     |     
+          0  |  X  |  0
+        _____|_____|_____
+             |     |     
+          X  |  X  |  0  
+        _____|_____|_____
+             |     |     
+          0  |  X  |  X  
+             |     |     
+
+    """,
+    """
+    Each players will take turn to put their element to the grid.  
+    The first player to have a consecutive element in a row win.
+    """,
+    """
+    Have Fun…
+    """,
+]
